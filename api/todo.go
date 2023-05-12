c355743fdf7da498dda0362f5d8c7e52db1b11f0
fabriziopandini <fpandini@vmware.com>
2023-05-12 17:44:35 +0200

    notes on next steps

---
+++ api/todo.go
@@ -0,0 +1,21 @@
+package api
+
+/*
+
+Define API types + templates:
+- GoofyCluster
+- GoofyMachine
+- GoofyVM
+
+GoofyCluster
+- Abide to the contract --> have controlPlaneEndpoint, status.Ready, etc.
+- TBD provider specific fields
+
+GoofyMachine
+- Abide to the contract --> have ProviderID, status.Ready, etc.
+- TBD provider specific fields
+
+GoofyVM
+- TBD if to put this in a separated API group to keep it cleaner
+
+*/
